09c098a68318ab41f250fdba9b4481c27335c763
qni <qni>
2024-01-23 06:24:00 +0700

    add test, finish

--- src/main.cpp
+++ src/main.cpp
@@ -1,154 +1,91 @@
 #include <iostream>
 #include <vector>
-#include "s21_vector.h"
+
+#include "lib/s21_vector.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-
-
 int main() {
-    // std::vector<int> numbers;
+  // std::vector<int> numbers;
 
-    // vector<int> f(5);
-    vector<int> v1 (4);
-    vector<int> v2 (4);
-    // v1.reserve(6);
+  // vector<int> f(5);
+  vector<int> v1(4);
+  vector<int> v2(4);
+  // v1.reserve(6);
 
-    v1[0] = 3;
-    v1[1] = 2;
-    v1[2] = 49;    
-    
-    v2[0] = 13;
-    v2[1] = 12;
-    v2[2] = 149;
+  v1[0] = 3;
+  v1[1] = 2;
+  v1[2] = 49;
 
-    // v1[3] = 58;
-    // auto my_iter = v1.begin();
-    // cout << my_iter << "my iter\n";
-    // auto my_iter2 = v2.begin();
-    // cout << my_iter2 << "my iter2\n";
+  v2[0] = 13;
+  v2[1] = 12;
+  v2[2] = 149;
 
-    // v1.print();
-    // auto tt = v1.capacity();
-    // cout << tt << endl;
+  auto hh = v1.capacity();
+  cout << hh << endl;
 
-    // auto jj = v1.size();
-    // cout << jj << endl;
+  auto rr = v1.size();
+  cout << rr << endl;
 
-    // v1.swap(v2);
+  vector<int>::iterator itz = v1.begin();
+  std::cout << "Type of num: " << typeid(itz).name() << std::endl;
+  // v1.print();
+  auto it = v1.insert_many(v1.begin(), 8, 9, 10, 12, 55);
+  cout << *it << " it\n";
 
-    //  my_iter = v1.begin();
-    // cout << my_iter << "my iter\n";
-    //  my_iter2 = v2.begin();
-    // cout << my_iter2 << "my iter2\n";
+  cout << endl;
 
-    // v1.pop_back();
-    // v1.pop_back();
-    // v1.pop_back();
-    // v1.push_back(87);
-    auto hh = v1.capacity();
-    cout << hh << endl;
+  cout << "eeeeeeeeee\n";
 
-    auto rr = v1.size();
-    cout << rr << endl;
+  //
+  std::vector<int> v4(3);
+  std::vector<char> v45{'f'};
+  cout << v45.max_size() << endl;
+  v4.reserve(6);
+  v4[0] = 5;
+  v4[1] = 6;
+  v4[2] = 7;
+  v4[5] = 9;
+  std::vector<int> cpv4(2);
+  cpv4[0] = 44;
+  cpv4 = std::move(v4);
+  // cout << v4.data() << "data\n";
+  for (int i : v4) std::cout << i << ' ';
+  cout << endl;
+  auto gg = v4.capacity();
+  cout << gg << endl;
+  auto zz = v4.size();
+  cout << zz << endl;
 
-    vector<int>::iterator itz = v1.begin();
-        std::cout << "Type of num: " << typeid(itz).name() << std::endl;
-    v1.print();
-    auto it = v1.insert_many(v1.begin(), 8, 9, 10, 12, 55);
-    cout << *it << " it\n";
+  for (int i : cpv4) std::cout << i << ' ';
+  cout << endl;
+  cout << cpv4[5] << "ctpv4\n";
+  auto mm = cpv4.capacity();
+  cout << mm << endl;
 
+  auto nn = cpv4.size();
+  cout << nn << endl;
 
+  cout << endl;
 
-    cout << endl;
+  auto iter = v4.insert(v4.end(), 99);
+  cout << *iter << "iter\n";
 
-// Expected equality of these values:
-//   v99.max_size()
-//     Which is: 9223372036854775807
-//   std_v99.max_size()
-//     Which is: 9223372036854775744
+  for (int i : v4) std::cout << i << "| ";
+  cout << endl;
 
-    cout << "eeeeeeeeee\n";
+  auto bb = v4.capacity();
+  cout << bb << endl;
 
-    // 
-    std::vector<int> v4 (3);
-    std::vector<char> v45 {'f'};
-    cout << v45.max_size() << endl;
-    v4.reserve(6);
-    v4[0] = 5;
-    v4[1] = 6;
-    v4[2] = 7;
-    v4[5] = 9;
-    std::vector<int> cpv4 (2);
-    cpv4[0] = 44;
-    cpv4 = std::move(v4);
-    // cout << v4.data() << "data\n";
-    for (int i: v4)
-        std::cout << i << ' ';
-    cout << endl;
-    auto gg = v4.capacity();
-    cout << gg << endl;
-    auto zz = v4.size();
-    cout << zz << endl;
+  auto aa = v4.size();
+  cout << aa << endl;
 
-
-    for (int i: cpv4)
-        std::cout << i << ' ';
-    cout << endl;
-    cout << cpv4[5] << "ctpv4\n";
-    auto mm = cpv4.capacity();
-    cout << mm << endl;
-
-    auto nn = cpv4.size();
-    cout << nn << endl;
-
-
-
-    cout << endl;
-
-
-    auto iter = v4.insert(v4.end(), 99);
-    cout << *iter << "iter\n";
-        // v4.pop_back();
-        // v4.pop_back();
-        // v4.pop_back();
-    //     v4.push_back(9);
-    //     v4.push_back(9);
-    //     v4.push_back(9);
-    // v4.clear();
-    
-    // v4.insert_many_back(4);
-    for (int i: v4)
-        std::cout << i << "| ";
-    cout << endl;
-
-
-    auto bb = v4.capacity();
-    cout << bb << endl;
-
-    auto aa = v4.size();
-    cout << aa << endl;
-
-    // std::vector<int>::iterator iter = v4.begin();
-    //     std::cout << "Type of num: " << typeid(iter).name() << std::endl;
-    // cout << *(iter++) << "begin" << endl;
-    // std::vector<int> v6;
-    // auto c = v6.front();
-    // cout << c;
-
-    // v4.at(3) = 55;
-    // auto b = v4.at(3);
-    // cout << b;
-    // auto a = v4.data();
-    // cout << *a;
-
-    // const std::type_info& type = typeid(v2);
-    // std::cout << type.name() << std::endl;
-    
-    
-
+  // std::vector<int>::iterator iter = v4.begin();
+  //     std::cout << "Type of num: " << typeid(iter).name() << std::endl;
+  // cout << *(iter++) << "begin" << endl;
+  // std::vector<int> v6;
+  // auto c = v6.front();
+  // cout << c;
 }
-
-
--- src/tests/test_vector.cc
+++ src/tests/test_vector.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include <vector>
-#include "../s21_vector.h"
+#include "../lib/s21_vector.h"
 
 TEST(vector, base) {
     vector<int> v1;
@@ -126,7 +126,81 @@ TEST(vector, insert) {
     ASSERT_EQ(vec.capacity(), std_vec.capacity());
 }
 
+TEST(vector, push_pop) {
+    vector<int> vec {1, 5, 6, 9};
+    vec.reserve(5);
+    std::vector<int> std_vec {1, 5, 6, 9};
+    std_vec.reserve(5);
 
+
+    vec.push_back(34);
+    vec.push_back(11);
+    std_vec.push_back(34);
+    std_vec.push_back(11);
+
+    ASSERT_EQ(vec.at(0), std_vec.at(0));
+    ASSERT_EQ(vec.at(1), std_vec.at(1));
+    ASSERT_EQ(vec.at(2), std_vec.at(2));
+    ASSERT_EQ(vec.at(3), std_vec.at(3)); 
+    ASSERT_EQ(vec.at(4), std_vec.at(4));
+    ASSERT_EQ(vec.at(5), std_vec.at(5));
+
+    ASSERT_EQ(vec.size(), std_vec.size());
+    ASSERT_EQ(vec.capacity(), std_vec.capacity());
+
+    vec.pop_back();
+    vec.pop_back();
+    std_vec.pop_back();
+    std_vec.pop_back();
+
+    ASSERT_EQ(vec.at(0), std_vec.at(0));
+    ASSERT_EQ(vec.at(1), std_vec.at(1));
+    ASSERT_EQ(vec.at(2), std_vec.at(2));
+    ASSERT_EQ(vec.at(3), std_vec.at(3)); 
+    ASSERT_EQ(vec.size(), std_vec.size());
+    ASSERT_EQ(vec.capacity(), std_vec.capacity());
+
+}
+
+TEST(vector, erase_swap) {
+    vector<int> vec {1, 5, 6, 9};
+    vec.reserve(5);
+    std::vector<int> std_vec {1, 5, 6, 9};
+    std_vec.reserve(5);
+
+
+    vec.erase(vec.begin() + 1);
+    std_vec.erase(std_vec.begin() + 1);
+
+    ASSERT_EQ(vec.at(0), std_vec.at(0));
+    ASSERT_EQ(vec.at(1), std_vec.at(1));
+    ASSERT_EQ(vec.at(2), std_vec.at(2));
+    
+    ASSERT_EQ(vec.size(), std_vec.size());
+    ASSERT_EQ(vec.capacity(), std_vec.capacity());
+
+
+    vector<int> vec_copy {32, 1, 7};
+    std::vector<int> std_copy {32, 1, 7};
+
+    vec.swap(vec_copy);
+    std_vec.swap(std_copy);
+
+    ASSERT_EQ(vec.at(0), std_vec.at(0));
+    ASSERT_EQ(vec.at(1), std_vec.at(1));
+    ASSERT_EQ(vec.at(2), std_vec.at(2));
+    
+    ASSERT_EQ(vec.size(), std_vec.size());
+    ASSERT_EQ(vec.capacity(), std_vec.capacity());
+
+    ASSERT_EQ(vec_copy.at(0), std_copy.at(0));
+    ASSERT_EQ(vec_copy.at(1), std_copy.at(1));
+    ASSERT_EQ(vec_copy.at(2), std_copy.at(2));
+    
+    ASSERT_EQ(vec_copy.size(), std_copy.size());
+    ASSERT_EQ(vec_copy.capacity(), std_copy.capacity());
+
+}
 
 int main(int argc, char** argv) {
   testing::InitGoogleTest(&argc, argv);
